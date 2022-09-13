f5d84ba884dee0a01354d9da15c23aea97cae776
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-09-13 20:29:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+let arr = [];
+let count;
+
+do {
+    count = +prompt(`Enter value between 2 and 10 (including)`);
+    if (count < 0) count = Math.abs(count);
+    if (!Number.isInteger(count)) count = Math.round(count);
+
+} while (!(count <= 10 && count >= 2));
+
+for (let i = 0; i < count; i++) {
+    let randomValue = Math.round(Math.random() * 10);
+    arr.push(randomValue);
+}
+console.log(arr);
